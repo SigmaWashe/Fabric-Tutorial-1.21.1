64103ace0ca83140a3efd6da3e21a305e043c3ad
SigmaWashe <chinasanwosu0@gmail.com>
2024-12-12 16:57:57 +0200

    Initial commit

---
+++ src/main/java/net/sigmawashe/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.sigmawashe.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
